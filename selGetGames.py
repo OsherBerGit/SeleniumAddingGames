b81ba7fdf171b1431964b9f0093f05c2eea4dd19
OsherBerGit <bergerosher10@gmail.com>
2025-02-19 23:22:05 +0200

    Tried to fix get games

--- selGetGames.py
+++ selGetGames.py
@@ -17,39 +17,34 @@ def interact_with_form():
 
     try:
         driver.get("https://store.steampowered.com/")
-        time.sleep(5)
+        time.sleep(3)
 
         wait = WebDriverWait(driver, 10)
         
         topSellers = driver.find_element(By.CLASS_NAME, "tab_content")
         driver.execute_script("arguments[0].scrollIntoView(true);", topSellers)
-        
         data = []
-        game_links = topSellers.find_elements(By.TAG_NAME, "a")
-        
-        for i, game in enumerate(game_links[:3]):  # חפש 3 משחקים בלבד
-            game_link = game.get_attribute("href")
-
-            if not game_link:
-                continue
-
-            driver.get(game_link)  # עבור לדף המשחק
-            time.sleep(2)
+        for i in range(2):
+            # game_link = game.get_attribute("href")
+            # if not game_link:
+            #     continue
+            # driver.get(game_link)
+            # time.sleep(2)
 
             try:
-                name = driver.find_element(By.ID, "appHubAppName").text
-                author = driver.find_element(By.ID, "developers_list").text
-                release_date = driver.find_element(By.CLASS_NAME, "date").text
-                genre = driver.find_element(By.CLASS_NAME, "app_tag").text
+                name = driver.find_element(By.CLASS_NAME, "tab_item_name").text
+                # author = driver.find_element(By.ID, "developers_list").text
+                # release_date = driver.find_element(By.CLASS_NAME, "date").text
+                genre = driver.find_element(By.CLASS_NAME, "discount_final_price").text
                 price = driver.find_element(By.CLASS_NAME, "game_purchase_price").text
-                img_url = driver.find_element(By.CLASS_NAME, "game_header_image_full").get_attribute("src")
-
-                data.append([name, author, release_date, genre, price, img_url])
+                img_url = driver.find_element(By.CLASS_NAME, "tab_item_cap_img").get_attribute("src")
+                print("3")
+                data.append([name, '', '', genre, price, img_url])
 
             except Exception as e:
                 print(f"Error scraping game {i+1}: {str(e)}")
 
-            driver.back()
+            # driver.back()
             time.sleep(2)
 
         df = pd.DataFrame(data, columns=['Name', 'Author', 'Release Date', 'Genre', 'Price', 'img_url'])
@@ -59,10 +54,10 @@ def interact_with_form():
             print(i)
 
     except Exception as e:
-        print(f"An error occurred: {str(e)}")  # Print any errors that occur
+        print(f"An error occurred: {str(e)}")
 
     finally:
-        driver.quit()  # Close the browser, regardless of success or failure
+        driver.quit()
 
 # Script entry point
 # Only run if this file is run directly (not imported)
